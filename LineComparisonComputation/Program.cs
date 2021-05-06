2f15668057a73ab64406b0f255738c6a4d7433fc
Arshad <arshadpinjari666@gmail.com>
2021-05-06 15:44:18 +0530

    Added.lineComparison

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -8,20 +8,19 @@ namespace LineComparisonComputation
         {
 
             {
-                int x3 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("x3:" + x3);
-                int y3 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("y3:" + y3);
-                Console.WriteLine("x3:" + x3 + "y3:" + y3);
-                int x4 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("x4:" + x4);
-                int y4 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("y4:" + y4);
-                Console.WriteLine("x4:" + x4 + "y4:" + y4);
-
-                double length2 = Math.Sqrt(Math.Pow((y4 - y3), 2) + Math.Pow((x4 - x3), 2));
-                Console.WriteLine("length of line 2:" + length2);
-
+                if (length1 <= length2)
+                {
+                    Console.WriteLine("line1 is smaller than line2");
+                }
+                else
+                {
+                    Console.WriteLine("line2 is greater");
+                }
+                if (length1 == length2)
+                {
+                    Console.WriteLine("line1 is equal line2");
+                }
+                
 
             }
         }
