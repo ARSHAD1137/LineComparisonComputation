14bbb35264858f598dfe78fcb58c1da89089f9f2
Arshad <arshadpinjari666@gmail.com>
2021-05-06 15:30:29 +0530

    Added.line1

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -6,7 +6,28 @@ namespace LineComparisonComputation
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to Line Computation");
+            Console.WriteLine("Enter the co-ordinates");
+            {
+                int x1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("x1:" + x1);
+                int y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("y1:" + y1);
+                Console.WriteLine("x1:" + x1 + "y1:" + y1);
+
+                int x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("x2:" + x2);
+                int y2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("y2:" + y2);
+                Console.WriteLine("x2:" + x2 + "y2:" + y2);
+
+              
+                double length1 = Math.Sqrt(Math.Pow((y2 - y1), 2) + Math.Pow((x2 - x1), 2));
+                Console.WriteLine("length of line 1:" + length1);
+
+                
+            }
         }
     }
 }
